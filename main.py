fd88ebc8cddfeb183dab165455b7de8c38c04c65
Sergey Piliavskiii <pill.sv0@gmail.com>
2019-12-02 15:41:22 +0200

    Ref [MISC]: finish second p2

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from lib.executor import execute
-from second.main import fixer
+from second.main import fixer, find_noun_and_verb
 print('hello')
 fixer()
+
+print('find noun and verb')
+noun, verb = find_noun_and_verb()
+print(f'noun {noun}, verb: {verb}, ret: {100 * noun + verb}')
